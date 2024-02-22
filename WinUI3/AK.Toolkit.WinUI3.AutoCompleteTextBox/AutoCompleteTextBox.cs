1126604739c7097888b297dec2cb0830d0b031e7
hl5ugc <hl5ugc@nate.com>
2024-02-22 10:45:53 +0900

    Add AutoCompleteTextBox custom control

---
+++ WinUI3/AK.Toolkit.WinUI3.AutoCompleteTextBox/AutoCompleteTextBox.cs
@@ -0,0 +1,14 @@
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AK.Toolkit.WinUI3.AutoCompleteTextBox;
+
+public sealed class AutoCompleteTextBox : TextBox
+{
+    public AutoCompleteTextBox()
+    {
+        this.DefaultStyleKey = typeof(AutoCompleteTextBox);
+    }
+}
